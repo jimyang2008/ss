218d1935b4ef701b7c07473df7b19c2dc59509ea
Jian Yang <pub@yangjian.net>
2017-11-19 12:16:18 +0000

    remove libsodium file and download it on the fly instead

--- go.sh
+++ go.sh
@@ -23,7 +23,8 @@ then
 else
     echo '- Install ChaCha20 encryption'
     yum install -y m2crypto gcc
-    tar -xzf files/libsodium-1.0.11.tar.gz
+    curl -LOk https://download.libsodium.org/libsodium/releases/libsodium-1.0.15.tar.gz
+    tar -xzf libsodium-1.0.15.tar.gz
     cd libsodium-*
     ./configure
     make && make install
