06645fce5bdbc42f6c2d9501d95b19bc230f169b
jimyang2008 <pub@yangjian.net>
2019-06-28 16:37:02 +0800

    Update install_ss.sh
    libsodium updated their download URL for old releases with an extra old/ in it

--- install_ss.sh
+++ install_ss.sh
@@ -42,7 +42,7 @@ install_ubuntu-16.04() {
 
 install_libsodium() {
     export LIBSODIUM_VER=1.0.15
-    curl -LOk https://download.libsodium.org/libsodium/releases/libsodium-$LIBSODIUM_VER.tar.gz
+    curl -LOk https://download.libsodium.org/libsodium/releases/old/libsodium-$LIBSODIUM_VER.tar.gz
     tar -xzf libsodium-$LIBSODIUM_VER.tar.gz
     pushd libsodium-$LIBSODIUM_VER
     ./configure --prefix=/usr && make
